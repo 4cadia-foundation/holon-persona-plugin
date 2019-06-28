bd367cd882f76d22dec2198e02c745d10748d880
Paulo Gabriel <paulo@janusproj.com>
2019-06-28 14:34:18 -0300

    Implemented web3 for connection in blockchain

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,6 +1,67 @@
 import InPage from './inpage';
 
-const inpage = new InPage();
+(async () => {
+
+  const inpage = new InPage();
+  const web3 = inpage.getInstance();
+
+  let nonce = await inpage.getNonce('0xdA26a8415346642850066f9912594E9b0f2662B2');
+
+  let options = {
+    from: '0xdA26a8415346642850066f9912594E9b0f2662B2',
+    gasPrice: '20000000000',
+    nonce: nonce,
+    gas: '4419583',
+    to: '0x80D475E1756ef1F2E918417AC4f9ae03E50CE59C',
+    value: '',
+    data: ''
+  };
+  debugger;
+  web3.eth.signTransaction(options).then((response) => {
+    console.log(response);
+  });
+
+})();
+
+
+//privatekey = B46A877F1BD0ABF2F30367AF0C4866B256DF87E5CF6E36AD4F25B7CA7465C661
+// const privateKey = new Buffer('B46A877F1BD0ABF2F30367AF0C4866B256DF87E5CF6E36AD4F25B7CA7465C661', 'hex');
+//
+
+
+
+// const rawTx = {
+//   nonce: '0x00',
+//   gasPrice: '0x09184e72a000',
+//   gasLimit: '0x2710',
+//   to: '0x0000000000000000000000000000000000000000',
+//   value: '0x00',
+//   data: '0x7f7465737432000000000000000000000000000000000000000000000000000000600057'
+// }
+
+
+
+//
+// let options = {
+//   from: '0xdA26a8415346642850066f9912594E9b0f2662B2',
+//   gasPrice: '20000000000',
+//   gas: '4419583',
+//   to: '0x80D475E1756ef1F2E918417AC4f9ae03E50CE59C',
+//   value: '',
+//   data: ''
+// };
+// debugger;
+// web3.eth.signTransaction(options).then(console.log);
+//
+//
+//
+// // contract.addInfoCategory(1, 'exemplo', {from: '0xdA26a8415346642850066f9912594E9b0f2662B2'}, (error, result) => {
+// //
+// //   if (!error)
+// //     console.log(result);
+// //
+// // });
+
 
 
 chrome.runtime.onInstalled.addListener(details => {
--- app/scripts.babel/inpage.js
+++ app/scripts.babel/inpage.js
@@ -1,7 +1,7 @@
 import Web3 from 'web3';
 import Config from './config/abi';
 import Settings from './config/settings';
-
+import NETWORK from './enums/network';
 
 class InPage {
 
@@ -16,6 +16,8 @@ class InPage {
     this._options.host = Settings.host || '127.0.0.1' ;
     this._options.port = Settings.port || '8545';
     this._options.provider = Settings.provider || 'https';
+    this._options.network = Settings.network || NETWORK.RINKEBYTESTNETWORK;
+
     Object.freeze(this._options);
 
     this._web3Instance = this.instance(this._options);
@@ -31,12 +33,17 @@ class InPage {
 
       if (!options)
         throw  new Error('option is not defined');
-
-      return new Web3(new Web3.providers.HttpProvider(`${options.provider}://${options.host}:${options.port}`));
+      debugger;
+      return new Web3(new Web3.providers.HttpProvider(`${options.provider}://${options.host}:${options.port}`), options.network);
 
     } catch(exception){
       console.error('[Inpage-instance] Error: ' + exception.message);
     }
+  }
+
+
+  getInstance() {
+    return this._web3Instance;
   }
 
   /**
@@ -49,6 +56,20 @@ class InPage {
       return register.at(Config.address);
     } catch (exception) {
       console.error('[Inpage-constract] Error: ' + exception.message)
+    }
+  }
+
+
+  async getNonce (address) {
+    try{
+
+      if (!address)
+        throw new Error('address not defined');
+
+      let nonce = await this.getInstance().eth.getTransactionCount(address);
+      return nonce;
+    } catch (exception) {
+      console.error('[Inpage - getNounce]' + exception.message);
     }
   }
 
