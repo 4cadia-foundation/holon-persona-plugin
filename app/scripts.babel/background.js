9e62b01555c66a0e32c9a34bf64b6e8aa771e4c5
Paulo Gabriel <paulo@janusproj.com>
2019-07-01 11:42:02 -0300

    Implemented SmartContract instance

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,67 +1,9 @@
-import InPage from './inpage';
-
-(async () => {
-
-  const inpage = new InPage();
-  const web3 = inpage.getInstance();
-
-  let nonce = await inpage.getNonce('0xdA26a8415346642850066f9912594E9b0f2662B2');
-
-  let options = {
-    from: '0xdA26a8415346642850066f9912594E9b0f2662B2',
-    gasPrice: '20000000000',
-    nonce: nonce,
-    gas: '4419583',
-    to: '0x80D475E1756ef1F2E918417AC4f9ae03E50CE59C',
-    value: '',
-    data: ''
-  };
-  debugger;
-  web3.eth.signTransaction(options).then((response) => {
-    console.log(response);
-  });
-
-})();
+import SmartContract from './smartcontract';
 
 
-//privatekey = B46A877F1BD0ABF2F30367AF0C4866B256DF87E5CF6E36AD4F25B7CA7465C661
-// const privateKey = new Buffer('B46A877F1BD0ABF2F30367AF0C4866B256DF87E5CF6E36AD4F25B7CA7465C661', 'hex');
-//
-
-
-
-// const rawTx = {
-//   nonce: '0x00',
-//   gasPrice: '0x09184e72a000',
-//   gasLimit: '0x2710',
-//   to: '0x0000000000000000000000000000000000000000',
-//   value: '0x00',
-//   data: '0x7f7465737432000000000000000000000000000000000000000000000000000000600057'
-// }
-
-
-
-//
-// let options = {
-//   from: '0xdA26a8415346642850066f9912594E9b0f2662B2',
-//   gasPrice: '20000000000',
-//   gas: '4419583',
-//   to: '0x80D475E1756ef1F2E918417AC4f9ae03E50CE59C',
-//   value: '',
-//   data: ''
-// };
-// debugger;
-// web3.eth.signTransaction(options).then(console.log);
-//
-//
-//
-// // contract.addInfoCategory(1, 'exemplo', {from: '0xdA26a8415346642850066f9912594E9b0f2662B2'}, (error, result) => {
-// //
-// //   if (!error)
-// //     console.log(result);
-// //
-// // });
-
+//instanciando as informações que são utilizadas nas página
+const smartContract = new SmartContract();
+const contract = smartContract.contract;
 
 
 chrome.runtime.onInstalled.addListener(details => {
@@ -69,3 +11,5 @@ chrome.runtime.onInstalled.addListener(details => {
 });
 
 chrome.browserAction.setBadgeText({text: '\'Olá'});
+
+
--- app/scripts.babel/smartcontract.js
+++ app/scripts.babel/smartcontract.js
@@ -1,16 +1,17 @@
-import Web3 from 'web3';
-import Config from './config/abi';
+// import Config from './config/abi';
 import Settings from './config/settings';
 import NETWORK from './enums/network';
+import {ethers} from 'ethers';
+import axios from 'axios';
 
-class InPage {
+class SmartContract {
 
 
   /**
    * create instance for smart contrat
    * @param {object} opts - option to initiate a smart contract
   */
-  constructor(opts = new Object()){
+  constructor(opts = new Object()) {
 
     this._options = new Object();
     this._options.host = Settings.host || '127.0.0.1' ;
@@ -18,62 +19,64 @@ class InPage {
     this._options.provider = Settings.provider || 'https';
     this._options.network = Settings.network || NETWORK.RINKEBYTESTNETWORK;
 
+
     Object.freeze(this._options);
 
-    this._web3Instance = this.instance(this._options);
+    this._instance = this._initialize(this._options);
+
+
   }
 
   /**
-  * function for inicializate web3 provider
+  * function for inicializate
   * @param {Object} options - options for instance inicializate
   * @return {Object} instance the web3 connection
-  * */
-  instance(options) {
-    try{
-
+  **/
+  _initialize(options) {
+    try {
       if (!options)
         throw  new Error('option is not defined');
-      debugger;
-      return new Web3(new Web3.providers.HttpProvider(`${options.provider}://${options.host}:${options.port}`), options.network);
+        return new ethers.providers.JsonRpcProvider(`${options.provider}://${options.host}:${options.port}`);
 
-    } catch(exception){
+    } catch(exception) {
       console.error('[Inpage-instance] Error: ' + exception.message);
     }
   }
 
-
-  getInstance() {
-    return this._web3Instance;
+  /**
+   * function for getInstance
+   * @return {Object} contract instance
+   * */
+  get instance() {
+    return this._instance;
   }
 
   /**
   * function for instance ethereum contract
   * @return {Object} contract instance
   * */
-  contract() {
+  get contract() {
     try {
-      let register = this._web3Instance.eth.contract(Config.abi);
-      return register.at(Config.address);
+      return this._config((response) => {
+        new ethers.Contract(response.address, response.abi, this._instance);
+      });
     } catch (exception) {
       console.error('[Inpage-constract] Error: ' + exception.message)
     }
   }
 
-
-  async getNonce (address) {
-    try{
-
-      if (!address)
-        throw new Error('address not defined');
-
-      let nonce = await this.getInstance().eth.getTransactionCount(address);
-      return nonce;
-    } catch (exception) {
-      console.error('[Inpage - getNounce]' + exception.message);
-    }
+  _config (callback) {
+    return axios.get('./config/abi.json')
+      .then((response) => {
+        return callback(response.data);
+      })
+      .catch((exception) => {
+        console.error('[SmartContract] Error: ' + exception);
+      });
   }
+
 
 }
 
-module.exports = InPage;
+module.exports = SmartContract;
 
