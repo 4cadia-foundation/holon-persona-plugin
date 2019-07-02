27a410e11a13ea699dc584014160a37b11e911a2
Paulo Gabriel <paulo@janusproj.com>
2019-07-02 10:56:33 -0300

    Config transaction assign with private key.

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,15 +1,22 @@
+/**
+ * Import class
+ **/
 import Transaction from './core/transaction';
 
 (async () => {
 
+  /*inject modules core*/
   const smartContract = new Transaction();
   const contract = await smartContract.contract();
-  const privateKey = '';
 
-  //exemplo
-  let transaction = await smartContract.transactionContractWithSigner(privateKey, contract,'addInfoCategory', [105, 'Exemplo4']);
+  debugger;
+
+  let transaction = await smartContract.transactionContractWithSigner('B46A877F1BD0ABF2F30367AF0C4866B256DF87E5CF6E36AD4F25B7CA7465C661', contract,'addInfoCategory', [104, 'Exemplo']);
 
   console.log(transaction);
 
+
 })();
+
+
 
