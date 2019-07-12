158358865abf638503688c837ae29758ef1dcb97
Victoria Duraes <victoria@janusproj.com>
2019-07-12 15:03:18 -0300

    Finalização da tel Welcome

--- app/src/modules/Welcome/Welcome.js
+++ app/src/modules/Welcome/Welcome.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react'
-import { Button } from 'react-bootstrap';
+import { Button, Grid, Row, Col } from 'react-bootstrap';
 import logo from '../../../images/logo.png';
+import './Welcome.css';
 
  class Welcome extends Component {
 
   render () {
 
     return (
-      <main>
-        <img className="logo" src={logo} alt="Logo" />
-        <h1 className="Title">Welcome to Holon</h1>
-        <h3 className="Subtitle">Connecting you to the Decentralized Web. We're happy to see you.</h3>
-        <Button bsStyle="warning">Get started</Button>
-      </main>
+        <Grid id="gridPrincipal">
+          <header>
+            <Row className="text-center">
+              <img className="logo" src={logo} alt="Logo" />
+            </Row>
+            <Row className="text-center">
+              <h3 className="Title">Welcome to Holon</h3>
+            </Row>
+          </header>
+          <main>
+          <Row className="text-center">
+            <p className= "Subtitle">Connecting you to the Decentralized Web. <br/> We're happy to see you.</p>
+          </Row>
+          </main>
+          <Row className="text-center" id="rowButton">
+            <footer>
+              <Button bsStyle="warning" className="text-center">Get started</Button>
+            </footer>
+          </Row>
+        </Grid>
     );
   }
 
