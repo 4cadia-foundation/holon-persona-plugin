b9f7c67af25b27bf4e3416af22d2a661e61e3c0d
Paulo Tarso <paulotarso@janusproj.com>
2019-07-15 15:41:58 -0300

    Revert "Tela de Welcome"
    This reverts commit 6048c6a9f0c68add9d689fdab38fb84e7e87db26.

--- app/src/index.js
+++ app/src/index.js
@@ -3,7 +3,6 @@ import store from './redux/store';
 import { Provider } from 'react-redux';
 import React, {Component} from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import WelcomeBack from './modules/WelcomeBack/WelcomeBack';
 
 import Welcome from './modules/Welcome/Welcome';
 import ImportWallet from './modules/ImportWallet/ImportWallet';
@@ -25,7 +24,7 @@ class App extends Component {
         <Provider store={store}>
           <Router>
             <Switch>
-              <Route path='' component={ WelcomeBack }></Route>
+              <Route path='' component={ Welcome }></Route>
               <Route path='/importwallet' component={ ImportWallet }></Route>
             </Switch>
           </Router>
