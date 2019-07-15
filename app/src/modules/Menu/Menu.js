46c855be16054d10acea4dd7ebd7e773844b975d
Yasmim Vieira <yasmim@janusproj.com>
2019-07-15 15:18:04 -0300

    Tela de menu em progresso.

---
+++ app/src/modules/Menu/Menu.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import { Row, Col, Grid } from 'react-bootstrap';
+import './Menu.css';
+
+class Menu extends Component {
+    render() {
+        return(
+        <Grid>
+            <Row>
+                <Col>
+                    <h1 className="text-center">SCREEN IN PROGRESS</h1>
+                    <iframe src="https://giphy.com/embed/3o7qDKusPtjf5FvlD2" width="300" height="300" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+                    <p><a href="https://giphy.com/gifs/angrybirds-red-angry-birds-movie-3o7qDKusPtjf5FvlD2"></a></p>
+                </Col>
+            </Row>
+        </Grid>
+)}};
+
+export default Menu;
