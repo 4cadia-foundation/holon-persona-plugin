60fac5abe63fce362cfb0f3b25ddbce23d635419
Beatriz Nonato <bia_nonato@icloud.com>
2019-07-15 18:16:55 -0300

    criar tela de identidade

---
+++ app/src/modules/CreateIdentity/CreateIdentity.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import { Grid, Row, Col, FormGroup, FormControl, ControlLabel, Button } from 'react-bootstrap';
+
+class CreateIdentity extends Component {
+    
+    constructor(props) {
+        super(props);
+    }
+
+    render () {
+        return (
+            <Grid>
+                <Row>
+                    <Col xs={12} md={12}>
+                            <div className="text-center">
+                                <h3>Holon Identity</h3>
+                            </div>
+                    </Col>
+                </Row>
+                <div className="margin-top-50">
+                    <Row>
+                        <p className= "Subtitle, text-center">To have a <b>Holon Identity</b>, please provide us with the following information.</p>
+                    </Row>
+                </div>
+                <FormGroup className="margin-top-50">
+                    <ControlLabel>Name</ControlLabel>
+                    <FormControl componentClass="input" type="text" />
+                </FormGroup>
+                <FormGroup>
+                    <ControlLabel>Email</ControlLabel>
+                    <FormControl componentClass="input" type="email" placeholder="email@example.com" />
+                </FormGroup>
+                <Button className="margin-top-50" bsSize="large" block bsStyle="warning">Create ID</Button>
+            </Grid>
+        );
+    }
+}
+
+export default CreateIdentity;
