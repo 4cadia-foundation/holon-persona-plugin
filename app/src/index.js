bfc7b2403baa291d1377fc02157c61666f5f0e85
Paulo Gabriel <paulo@janusproj.com>
2019-07-16 11:20:32 -0300

    Implemented injection routes dynamic

--- app/src/index.js
+++ app/src/index.js
@@ -2,15 +2,14 @@ import ReactDOM from 'react-dom';
 import store from './redux/store';
 import { Provider } from 'react-redux';
 import React, {Component} from 'react';
-import { Route, Link, HashRouter , Switch } from "react-router-dom";
+import { Route, HashRouter , Switch } from "react-router-dom";
 
-import Navbar from './components/Navbar/Navbar';
 
-import Welcome from './modules/Welcome/Welcome';
-import ImportWallet from './modules/ImportWallet/ImportWallet';
+
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/main.css';
+import routers from "./router";
 
 
 class App extends Component {
@@ -18,12 +17,8 @@ class App extends Component {
   render (){
     return (
       <div className="hollonApp">
-
-       <Navbar />
-
        <Switch>
-          <Route path="/" exact={true} component={ Welcome } />
-          <Route path="/importwallet" component={ ImportWallet } />
+         {routers.map((item, index)=> (<Route key={index} path={item.path} component={item.component} exact={item.exact} />))}
        </Switch>
       </div>
     )
