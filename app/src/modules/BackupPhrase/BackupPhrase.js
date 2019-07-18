8a5b9242f4f67018444e174cabd7495b0f29c45a
Yasmim Vieira <yasmim@janusproj.com>
2019-07-18 17:34:34 -0300

    Reajuste dos códigos em ingles e retirada de espaços vagos.

--- app/src/modules/BackupPhrase/BackupPhrase.js
+++ app/src/modules/BackupPhrase/BackupPhrase.js
@@ -8,19 +8,18 @@ class BackupPhrase extends Component {
             <Grid>
                 <Row>
                     <Col>
-                        <h1 className="text-center titulo">Secret Backup Phrase</h1>
-                            <div className="paragrafo-explicacao">
+                        <h1 className="text-center title">Secret Backup Phrase</h1>
+                            <div className="paragraph-explication">
                                 <Glyphicon className="icon-alert" glyph="alert" />
                                 <p>Your <strong>secret backup phrase</strong> makes it easy restore your account.
                                 <strong> Never</strong> disclose your backup phrase.
                                 Anyone with this phrase can takes your Ether forever.</p>
                             </div>
-                        <textarea className="caixa-mnemonico"rows="10" cols="40" maxlength="50"></textarea>
-                        <div className="botoes-saida">
+                        <textarea className="box-mnemonico"rows="10" cols="40" maxlength="50"></textarea>
+                        <div className="button-exit">
                             <Button bsStyle="warning" >Back</Button>
                             <Button bsStyle="warning" >Next</Button>
                         </div>
-
                     </Col>
                 </Row>
             </Grid>
