18109c026bedd4754657be63ce6954e51d578860
Yasmim Vieira <yasmim@janusproj.com>
2019-07-19 14:40:28 -0300

    Telas De BackupPhrase que foram excluidas.

---
+++ app/src/modules/BackupPhrase/BackupPhrase.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import {Button, Grid, Col, Row, Glyphicon } from 'react-bootstrap';
+import './BackupPhrase.css'
+
+class BackupPhrase extends Component {
+    render() {
+        return (
+            <Grid>
+                <Row>
+                    <Col>
+                        <h1 className="text-center title">Secret Backup Phrase</h1>
+                            <div className="paragraph-explication">
+                                 <Glyphicon className="icon-alert" glyph="alert" />
+                                 <p>Your <strong>secret backup phrase</strong> makes it easy restore your account.
+                                 <strong> Never</strong> disclose your backup phrase.
+                                 Anyone with this phrase can takes your Ether forever.</p>
+                            </div>
+                            <textarea className="box-mnemonico"rows="10" cols="40" maxlength="50"></textarea>
+                            <div className="button-exit">
+                                <Button bsStyle="warning" >Back</Button>
+                                <Button bsStyle="warning" >Next</Button>
+                            </div>
+                     </Col>
+                 </Row>
+             </Grid>
+         )
+     }
+ }
+ 
+ export default BackupPhrase;
