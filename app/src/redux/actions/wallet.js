897995ae41bfa930adf5abdfdbe10bdb6de2a3f3
Paulo Tarso <paulotarso@janusproj.com>
2019-07-19 18:26:51 -0300

    Trabalho de 19/07

--- app/src/redux/actions/wallet.js
+++ app/src/redux/actions/wallet.js
@@ -1,7 +1,6 @@
 import Wallet  from '../../../scripts/core/WalletStorage';
 import { buildToast, ToastTypes} from '../../helper/toast';
 import * as ActionTypes from '../../constants/actionsTypes';
-
 const wallet = new Wallet();
 
 export function restoreVault(password, seed) {
@@ -20,5 +19,25 @@ export function restoreVault(password, seed) {
           toast: buildToast(exception.message, {type: ToastTypes.ERROR})
         });
       })
+  }
+}
+
+export function createNewWallet(password){
+  debugger;
+  return dispatch=> {
+    wallet.createNewWallet(password)
+    .then(vault => {
+      dispatch({
+        type: ActionTypes.SET_WALLET_CREATE,
+      //  accounts: vault.keyrings[0].accounts,
+        toast: buildToast('Wallet create with successful', {type: ToastTypes.SUCCESS})
+      });
+    })
+    .catch(exception => {
+      dispatch({
+        type: ActionTypes.SET_ACCOUNTS_ERROR,
+        toast: buildToast(exception.message, {type: ToastTypes.ERROR})
+      });
+    })
   }
 }
