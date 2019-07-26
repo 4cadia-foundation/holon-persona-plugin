5f732f705b1573392416cf273ca5eb434048dabd
Beatriz Nonato <bia_nonato@icloud.com>
2019-07-26 19:05:46 -0300

    changed for standard, title, paragraph, space and button

--- app/src/modules/Menu/Menu.js
+++ app/src/modules/Menu/Menu.js
@@ -1,11 +1,11 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Link, Redirect } from 'react-router-dom';
-import { Row, Col, Grid, Glyphicon, DropdownButton, MenuItem, Button} from 'react-bootstrap';
+import { Button, Col, DropdownButton, Glyphicon, Grid, MenuItem, Row } from 'react-bootstrap';
 
-import CloseIconPage from '../../components/CloseIconPage/CloseIconPage';
-import './Menu.css';
-import '../../styles/_utils.css';
 import Balance from '../../components/Balance/Balance';
+import CloseIconPage from '../../components/CloseIconPage/CloseIconPage';
+import '../../styles/_utils.css';
+import './Menu.css';
 
 class Menu extends Component {
   
@@ -35,72 +35,68 @@ class Menu extends Component {
         <Grid>
             <Row>
                 <Col>
-                    <nav className="d-flex flex-row justify-content-between">
-                        <p className="titleMenu">Identity</p>
-                        <CloseIconPage className="destination-icon" destination="/home"/>                    
-                    </nav>
-                    <hr className="line"/>
+                    <div className="btn-menu-close">
+                        <CloseIconPage destination="/home"/>
+                    </div>
+                    <div className="d-flex flex-row justify-content-between margin-top-10">
+                        <h3 id="title-menu" className="title">Identity</h3>
+                    </div>
+                    <hr className="line-menu"/>
                     <div className="links">
                         <div className="flex-column">
                             <Link to='/addinformation'>
-                                <Glyphicon glyph="plus"/> 
-                                <a className="icons">Add information</a>
+                                <Glyphicon id="glyph-color" glyph="plus"/> 
+                                <a className="space-icon-p paragraph">Add information</a>
                             </Link>
                         </div>
                         <div className="flex-column">
                             <Link to="/validateinformation">
-                                <Glyphicon glyph="ok"/>
-                                <a href="" className="icons">Validate information</a>
+                                <Glyphicon id="glyph-color" glyph="ok"/>
+                                <a href="" className="space-icon-p paragraph">Validate information</a>
                             </Link>
                         </div>
                         <div className="flex-column">
                             <Link to="/notifications">
-                                <Glyphicon glyph="download-alt"/>
-                                <a href="" className="icons">Notifications</a>
+                                <Glyphicon id="glyph-color" glyph="download-alt"/>
+                                <a href="" className="space-icon-p paragraph">Notifications</a>
                             </Link>
                         </div>
                     </div>
-                    <hr className="line" />
+                    <hr className="line-menu" />
                     <div className="links2">
                         <div className="flex-column">
                             <Link to="/profile">
-                                <Glyphicon glyph="user"/> 
-                                <a href="" className="icons">Profile</a>
+                                <Glyphicon id="glyph-color" glyph="user"/> 
+                                <a href="" className="space-icon-p paragraph">Profile</a>
                             </Link>
                         </div>
                         <div className="flex-column">
-                            <Glyphicon glyph="share"/>
-                            <a href="" className="icons">Etherscan</a>
+                            <Glyphicon id="glyph-color" glyph="share"/>
+                            <a href="" className="space-icon-p paragraph">Etherscan</a>
                         </div>
                         <div className="flex-column">
                             <Link to="/backupphrase">
-                                <Glyphicon glyph="lock"/>
-                                <a href="" className="icons">Secret Backup Phrase</a>
+                                <Glyphicon id="glyph-color" glyph="lock"/>
+                                <a href="" className="space-icon-p paragraph">Secret Backup Phrase</a>
                             </Link>
                         </div>
                         <div  className="flex-column">
-                            <Glyphicon glyph="cog"/>
+                            <Glyphicon id="glyph-color" glyph="cog"/>
                                 <DropdownButton
                                     bsSize="xsmall"
                                     title="Select network"
-                                    id="drop">
-                                    <MenuItem eventKey="1">Main ethereum network</MenuItem>
-                                    <MenuItem eventKey="2">Rinkeby network</MenuItem>
-                                    <MenuItem eventKey="3">Localhost</MenuItem>
+                                    id="dropdown">
+                                    <MenuItem className="paragraph" eventKey="1">Main ethereum network</MenuItem>
+                                    <MenuItem className="paragraph" eventKey="2">Rinkeby network</MenuItem>
+                                    <MenuItem className="paragraph" eventKey="3">Localhost</MenuItem>
                                 </DropdownButton>
                         </div>
                     </div>
-                    <hr className="line"></hr>
+                    <hr className="line-menu"></hr>
                     <div>
                         <Balance />
                     </div>
-                    <div className="botoes">
-                      <Button bsStyle="warning">Deposit</Button>
-                      <Button bsStyle="warning">Send</Button>
-                    </div>
-                    <Link to="/welcomeback">
-                        <a href="" className="sair">Logout</a>
-                    </Link>
+                    <Button className="paragraph" bsSize="small" onClick={() => this.props.history.push('/welcomeback')}>Logout</Button>
                 </Col>
             </Row>
     </Grid>
