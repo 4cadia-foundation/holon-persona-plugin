c5df0500ab6a84b8ff2d383ff84197f9d9e878d4
Victor Hugo Ramos Fernandes de Santana <victor@janusproj.com>
2019-08-13 16:58:26 -0300

    add eventsService unit test

--- app/scripts/test/application/service/EventsService.test.js
+++ app/scripts/test/application/service/EventsService.test.js
@@ -1,4 +1,18 @@
 import EventsService from '../../../core/application/service/EventsService';
+import { letMeSeeYourDataSample } from '../../template/eventsSample'
+
+
+let _eventsService;
+
+beforeEach(() => {
+    let getLogsMock = jest.fn(async (filterData) => {
+        return letMeSeeYourDataSample;
+    });
+    let provider = {
+        getLogs: getLogsMock
+    };
+    _eventsService = new EventsService(provider);
+});
 
 describe("GetEventHash Tests", () => {
     it("No logs should returns null", async () => {
@@ -14,6 +28,11 @@ describe("GetEventHash Tests", () => {
         expect(hashResult).toBeNull();
     });
 
-
+    it("Returns 2 transactionHashes", async () => {
+        var hashes = await _eventsService.GetEventHash(null);
+        expect(hashes).toContain(letMeSeeYourDataSample[0].transactionHash);
+        expect(hashes).toContain(letMeSeeYourDataSample[1].transactionHash);
+        expect(hashes.length).toBe(2);
+    });
 });
 
