71b38c80ffe1d7adb3b7b820f7eb895f3f4f4ba4
vicduraes <duraes.vic@gmail.com>
2019-10-18 18:05:06 -0300

    HomeCard modification

--- app/src/components/HomeCard/HomeCard.js
+++ app/src/components/HomeCard/HomeCard.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 import {Label} from 'react-bootstrap';
-import { FaRegListAlt } from "react-icons/fa";
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as PersonaActions from '../../redux/actions/persona';
 
-import PropTypes from 'prop-types';
 import './HomeCard.css'
 class HomeCard extends Component{
 
@@ -80,66 +78,47 @@ class HomeCard extends Component{
 
     render() {
         const {persona} = this.state;
-        const enableDetail = this.state.enableDetail;
-        const { toggle, emitClick } = this.props;
 
         return(
-        <section className={ `card card-history col-sm-12 `}   onClick={ toggle? this.showDetail.bind( this ) : emitClick.bind( this ) }>
+            <div> 
+                {persona.personalInfo.map((item, index) => {
+                    let person = {...item, enable: false};
+                return ( <section key={index} className={ 'card card-history'} >
+            
+                    <header className='card-header' onClick={ () => {
+                        debugger; 
+                        person.enable = !person.enable}  }>
 
-            <header className='card-header'>
-
-                <FaRegListAlt className={'margin-left-10 hidden-xs'} size={32} />
-
-                <div className='box-info'>
-                {persona.personalInfo.map((item, index) => 
-                    <div key={index}>
-                        <h4 className='title' > { item.valor } </h4>
+                    <div className='box-info'>
+                        <div >
+                            <h4 className='title' > { person.valor } </h4>
+                        </div>
                     </div>
-                    )
-                }
-                </div>
-            </header>
+                    </header>
 
-            <section className={`row col-md-12 card-history-detail ${ enableDetail ? 'show' : 'hide'}`}>
+                <section className={'row col-md-12 card-history-detail' + (person.enable) ? 'show' : 'hide'}>
 
-             <div className='row'>
-                  {persona.personalInfo.map((item, index) => 
-                    <div key={index}>
-                        <div className='col-md-4'>
-                          <p> <span className="paragraph">Field: </span>{item.valor}</p>
-                        </div>
-                        <div className='col-md-4'>
-                            <p> <span className="paragraph">Status: </span> 
-                            <Label bsStyle={ this.getValidationDescClass(item.statusValidationCode) }>
-                              {item.statusValidationDescription}
-                            </Label></p>
-                        </div>
-                    </div>                
-                    )
-                }          
+                    <div className='row'>
+                            <div >
+                                <div className='col-md-4'>
+                                <p> <span className="paragraph">Field: </span>{person.valor}</p>
+                                </div>
+                                <div className='col-md-4'>
+                                    <p> <span className="paragraph">Status: </span> 
+                                    <Label bsStyle={ this.getValidationDescClass(person.statusValidationCode) }>
+                                    {person.statusValidationDescription}
+                                    </Label></p>
+                                </div>
+                            </div>                        
+                    </div>
+                </section>
+                </section>
+                )})}
             </div>
-            </section>
-        </section>
+
         )
     }
 }
-
-HomeCard.defaultProps = {
-    toggle: true,
-    personData: {
-        status: 'Not defined',
-        field: 'Not defined',
-    }
-};
-
-HomeCard.propTypes = {
-    toggle: PropTypes.bool,
-    personData: PropTypes.shape({
-            status: PropTypes.string,
-            field: PropTypes.string,
-        }),
-    emitClick: PropTypes.func
-};
 
 const mapStateToProps = state => ({ 
     persona: state.persona
